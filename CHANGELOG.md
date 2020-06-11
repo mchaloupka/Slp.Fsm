4bdf96a9d6c7692dedaf641976ffe37bda9adc64
Milos Chaloupka <chaloupkamilos@hotmail.com>
2020-06-12 00:38:32 +0100

    Bump version to 0.1.0-beta
    ## [0.1.0-beta] - 2020-06-12
    
    ### Added
    - Support for creation
    - Ability to remove lambda edges
    - Ability to remove non-reachable states

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,15 @@
 # Changelog
+
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+## [0.1.0-beta] - 2020-06-12
 
 ### Added
 - Support for creation
 - Ability to remove lambda edges
 - Ability to remove non-reachable states
+[Unreleased]: https://github.com/mchaloupka/Slp.Fsm/compare/v0.1.0-beta...HEAD
+[0.1.0-beta]: https://github.com/mchaloupka/Slp.Fsm/releases/tag/v0.1.0-beta
--- src/Slp.Fsm/AssemblyInfo.fs
+++ src/Slp.Fsm/AssemblyInfo.fs
@@ -5,19 +5,19 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Slp.Fsm")>]
 [<assembly: AssemblyProductAttribute("Slp.Fsm")>]
 [<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2017-03-17T00:00:00.0000000")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2020-06-12T00:00:00.0000000+01:00")>]
 [<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 [<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","bb8964b54bee133e9af64d316dc2cfee16df7f72")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseChannel","beta")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","c45983a54eb5c0995ba3ffa6ae8c506dbe8fa10b")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Slp.Fsm"
     let [<Literal>] AssemblyProduct = "Slp.Fsm"
     let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2017-03-17T00:00:00.0000000"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2020-06-12T00:00:00.0000000+01:00"
     let [<Literal>] AssemblyFileVersion = "0.1.0"
     let [<Literal>] AssemblyInformationalVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "bb8964b54bee133e9af64d316dc2cfee16df7f72"
+    let [<Literal>] AssemblyMetadata_ReleaseChannel = "beta"
+    let [<Literal>] AssemblyMetadata_GitHash = "c45983a54eb5c0995ba3ffa6ae8c506dbe8fa10b"
